70de8c1fb11694079cd18c7afd3a92707f8d120c
lsy233 <119912477+fhgyuanshen@users.noreply.github.com>
2025-03-31 19:58:29 +0800

    Update backbone.py

--- model/backbone.py
+++ model/backbone.py
@@ -9,7 +9,7 @@ import numpy as np
 import copy
 
 
-class CLIPViTFM2(nn.Module):  
+class CLIPViTFM(nn.Module):  
     def __init__(self, model_name='ViT-B/16', size=224):
         super().__init__()
 
@@ -122,28 +122,16 @@ class CLIPViTFM2(nn.Module):
         # attn_masks[:, 1:, 0] = pred_masks[:,None,:,:].repeat(1,self.num_heads,1,1).view(N * self.num_heads, -1)
         return ~attn_masks
     
-    def make_attn_mask_little(self, pred_masks, size=None):
-        # pred_masks =  [46, H, W], Torch.bool
-        if size is not None:
-            pred_masks = TF.resize(pred_masks, size=(size, size))  # [46,7,7]
-        # pred_masks = pred_masks.type(torch.bool)
-        N, H, W = pred_masks.size()
-        attn_masks = torch.ones((N * self.num_heads, H*W+1, H*W+1), dtype=torch.float32).to(self.device)
-        attn_masks[:, 0, 1:] = pred_masks[:,None,:,:].expand(-1,self.num_heads,-1,-1).reshape(N * self.num_heads, -1)
-        # attn_masks[:, 1:, 0] = pred_masks[:,None,:,:].repeat(1,self.num_heads,1,1).view(N * self.num_heads, -1)
-        # print(attn_masks-1)
-        return (attn_masks-1)*4
-    
-    def forward(self, masked_img, prompted_img, pred_masks,  masking_block=None, masking_type='share_attn_merge_g2l_tokenmask', layer_add=0):
+    def forward(self, local_imgs, global_imgs, pred_masks, masking_block=None, fusion_mode='G2L'):
         if masking_block is None:
             masking_block = self.last_layer
 
         vit = self.model.visual
 
-        x = masked_img.type(self.model.dtype)
+        x = local_imgs.type(self.model.dtype)
         
 
-        if masking_type == 'crop': # [1, 512]
+        if fusion_mode == 'crop': # [1, 512]
             x = vit(x)
             # print(x[:,0,:].shape)
             return x[:, 0, :]
@@ -164,8 +152,8 @@ class CLIPViTFM2(nn.Module):
 
         x = x.permute(1, 0, 2)  # NLD -> LND
         ####################### x2 ####################################
-        if prompted_img is not None:
-            x2 = prompted_img.type(self.model.dtype)
+        if global_imgs is not None:
+            x2 = global_imgs.type(self.model.dtype)
             x2 = vit.conv1(x2)  # shape = [*, width, grid, grid]
             x2 = x2.reshape(x2.shape[0], x2.shape[1], -1)
             x2 = x2.permute(0, 2, 1)  # shape = [*, grid ** 2, width]
@@ -177,8 +165,6 @@ class CLIPViTFM2(nn.Module):
 
             x2 = x2.permute(1, 0, 2)  # NLD -> LND
 
-
-
         L, N, D = x[1:, :, :].size(0), x.size(1), x.size(2)
         size = int(np.sqrt(L))
         assert size * size == L
@@ -186,7 +172,7 @@ class CLIPViTFM2(nn.Module):
         pred_masks = TF.resize(pred_masks.type(torch.float32), (size, size))
         # pred_masks = torch.stack([pred_mask.view(size,224//size,size,224//size).max(dim=1)[0].max(dim=2)[0] for pred_mask in pred_masks])
 
-        if masking_type == 'token_masking':
+        if fusion_mode == 'token_masking':
             for block_idx, resblock in enumerate(vit.transformer.resblocks):
                 if block_idx >= masking_block:
                     cls = x[:1,:,:]
@@ -212,7 +198,7 @@ class CLIPViTFM2(nn.Module):
                 else:
                     x = resblock(x)
 
-        elif masking_type == 'attn_masking':
+        elif fusion_mode == 'attn_masking':
             attn_mask = self.make_attn_mask(pred_masks)
             for block_idx, resblock in enumerate(vit.transformer.resblocks):
                 if block_idx >= masking_block:
@@ -221,7 +207,7 @@ class CLIPViTFM2(nn.Module):
                         x = x.expand(-1, N, -1)
 
                     x = resblock(x, attn_mask=attn_mask)
-                    # print(x.shape,block_idx)
+
                     if block_idx == self.last_layer:
                         x = x.permute(1, 0, 2) # [46, 50, 768]
                         x = self.model.visual.ln_post(x[:, 0, :]) # [46, 768]
@@ -231,22 +217,19 @@ class CLIPViTFM2(nn.Module):
                 else:
                     x = resblock(x)
 
-        elif masking_type == 'share_attn_merge_l2g':
+        elif fusion_mode == 'L2G':
             attn_mask = self.make_attn_mask(pred_masks)
-            # local
+            x1_x2 = torch.cat([x,x2], dim=1)
             for block_idx, resblock in enumerate(vit.transformer.resblocks): 
-                # print(block_idx,x.shape)
                 if block_idx >= masking_block:
-                    # x2 = resblock(x2,attn_weights=attn_weights)
-                    _x = x.clone()
-                    
+                    if block_idx == masking_block:
+                        x = x1_x2[:,:len(pred_masks),:]
+                        x2 = x1_x2[:,len(pred_masks):,:]
+                    x_ori_local = x.clone()
                     x = resblock(x)
-                    # x2 = resblock((_x+x2)/2, x_k=(_x+x2)/2, x_v=(_x+x2)/2,attn_mask=attn_mask)
-                    x2 = resblock(_x+x2*2,attn_mask=attn_mask) 
-                    # print(x2.shape,x.shape)
-                else:
-                    x = resblock(x) 
-                    x2 = resblock(x2)
+                    x2 = resblock(x_ori_local+x2*2,attn_mask=attn_mask) 
+                else:                    
+                    x1_x2 = resblock(x1_x2)
             
                 if block_idx == self.last_layer+1:
                     x2 = x2.permute(1, 0, 2) # [46, 50, 768]
@@ -256,37 +239,31 @@ class CLIPViTFM2(nn.Module):
                     return x2
                     # break
 
-        elif masking_type == 'share_attn_merge_g2l_tokenmask':
+        elif fusion_mode == 'G2L':
             attn_mask = self.make_attn_mask(pred_masks)
             x1_x2 = torch.cat([x,x2], dim=1)
-            # print(x1_x2.shape)
-            # local
             for block_idx, resblock in enumerate(vit.transformer.resblocks): 
-                # print(block_idx,x.shape)
                 if block_idx >= masking_block:
                     if block_idx == masking_block:
                         x = x1_x2[:,:len(pred_masks),:]
                         x2 = x1_x2[:,len(pred_masks):,:]
-                    _x2 = x2.clone()
-                    cls = _x2[:1,:,:]
-                    _x2 = _x2[1:,:,:]
-                    _x2 = _x2.permute(1,2,0) # [49, 1, 768] -> [1, 768, 49]
+                    x_ori_global = x2.clone()
+                    cls = x_ori_global[:1,:,:]
+                    x_ori_global = x_ori_global[1:,:,:]
+                    x_ori_global = x_ori_global.permute(1,2,0) # [49, 1, 768] -> [1, 768, 49]
 
-                    _x2 = _x2.view(N, D, size, size).contiguous() # [1, 768, 49] -> [1, 768, 7, 7]
+                    x_ori_global = x_ori_global.view(N, D, size, size).contiguous() # [1, 768, 49] -> [1, 768, 7, 7]
 
-                    _x2 = torch.mul(_x2, pred_masks[:, None, :, :]) # [46, 768, 7, 7]
-                    N = _x2.size(0)
-                    _x2 = _x2.view(N, D, L).contiguous() # [46, 768, 7, 7] -> [46, 768, 49]
+                    x_ori_global = torch.mul(x_ori_global, pred_masks[:, None, :, :]) # [46, 768, 7, 7]
+                    N = x_ori_global.size(0)
+                    x_ori_global = x_ori_global.view(N, D, L).contiguous() # [46, 768, 7, 7] -> [46, 768, 49]
 
-                    _x2 = _x2.permute(2,0,1) # [46, 768, 49] -> [49, 46, 768]
-                    _x2 = torch.cat([cls.expand(-1,N,-1), _x2], dim=0) # [50, 46, 768]
+                    x_ori_global = x_ori_global.permute(2,0,1) # [46, 768, 49] -> [49, 46, 768]
+                    x_ori_global = torch.cat([cls.expand(-1,N,-1), x_ori_global], dim=0) # [50, 46, 768]
 
-                    x = resblock(_x2*2+x) 
+                    x = resblock(x_ori_global*2+x) 
                     x2 = resblock(x2, attn_mask=attn_mask)
-                    # print(x2.shape,x.shape)
                 else:
-                    # x = resblock(x) 
-                    # x2 = resblock(x2)
                     x1_x2 = resblock(x1_x2)
             
                 if block_idx == self.last_layer+1:
@@ -297,49 +274,51 @@ class CLIPViTFM2(nn.Module):
 
                     return x
 
-        elif masking_type == 'share_attn_merge_gl2gl_new':
+        elif fusion_mode == 'G2L&L2G':
             attn_mask = self.make_attn_mask(pred_masks)
+            x1_x2 = torch.cat([x,x2], dim=1)
             # local
             for block_idx, resblock in enumerate(vit.transformer.resblocks): 
 
                 if block_idx >= masking_block:
-                    # clone x and x2
-                    _x0 = x.clone()#用来融入global
-                    _x20 = x2.clone()#用来token masking融入local
                     if block_idx == masking_block:
-                        _x1 = x.clone()#用来跑local
-                        _x21 = x2.clone()#用来attn masking跑global
+                        x = x1_x2[:,:len(pred_masks),:]
+                        x2 = x1_x2[:,len(pred_masks):,:]
+                        x_hybrid_local = x.clone()
+                        x_hybrid_global = x2.clone()
+
+                    x_ori_local = x.clone()
+                    x_ori_global = x2.clone()
                     # token masking
-                    cls = _x20[:1,:,:]
-                    _x20 = _x20[1:,:,:]
-                    _x20 = _x20.permute(1,2,0) # [49, 1, 768] -> [1, 768, 49]
-                    _x20 = _x20.view(N, D, size, size).contiguous() # [1, 768, 49] -> [1, 768, 7, 7]
-                    _x20 = torch.mul(_x20, pred_masks[:, None, :, :]) # [46, 768, 7, 7]
-                    N = _x20.size(0)
-                    _x20 = _x20.view(N, D, L).contiguous() # [46, 768, 7, 7] -> [46, 768, 49]
-                    _x20 = _x20.permute(2,0,1) # [46, 768, 49] -> [49, 46, 768]
-                    _x20 = torch.cat([cls.expand(-1,N,-1), _x20], dim=0) # [50, 46, 768]
+                    cls = x_ori_global[:1,:,:]
+                    x_ori_global = x_ori_global[1:,:,:]
+                    x_ori_global = x_ori_global.permute(1,2,0) # [49, 1, 768] -> [1, 768, 49]
+                    x_ori_global = x_ori_global.view(N, D, size, size).contiguous() # [1, 768, 49] -> [1, 768, 7, 7]
+                    x_ori_global = torch.mul(x_ori_global, pred_masks[:, None, :, :]) # [46, 768, 7, 7]
+                    N = x_ori_global.size(0)
+                    x_ori_global = x_ori_global.view(N, D, L).contiguous() # [46, 768, 7, 7] -> [46, 768, 49]
+                    x_ori_global = x_ori_global.permute(2,0,1) # [46, 768, 49] -> [49, 46, 768]
+                    x_ori_global = torch.cat([cls.expand(-1,N,-1), x_ori_global], dim=0) # [50, 46, 768]
 
                     x = resblock(x)
                     x2 = resblock(x2,attn_mask=attn_mask)
-                    _x1 = resblock(_x1+2*_x20) 
-                    _x21 = resblock(_x0+2*_x21, attn_mask=attn_mask)
-                    # print(x2.shape,x.shape)
+                    x_hybrid_local = resblock(x_hybrid_local+2*x_ori_global) 
+                    x_hybrid_global = resblock(x_ori_local+2*x_hybrid_global, attn_mask=attn_mask)
+                    
                 else:
-                    x = resblock(x) 
-                    x2 = resblock(x2)
+                    x1_x2 = resblock(x1_x2)
             
                 if block_idx == self.last_layer+1:
-                    _x1 = _x1.permute(1, 0, 2) # [46, 50, 768]
-                    _x1 = self.model.visual.ln_post(_x1[:, 0, :]) # [46, 768]
+                    x_hybrid_local = x_hybrid_local.permute(1, 0, 2) # [46, 50, 768]
+                    x_hybrid_local = self.model.visual.ln_post(x_hybrid_local[:, 0, :]) # [46, 768]
                     if self.model.visual.proj is not None:
-                        _x1 = _x1 @ self.model.visual.proj # [46, 512] 
+                        x_hybrid_local = x_hybrid_local @ self.model.visual.proj # [46, 512] 
 
-                    _x21 = _x21.permute(1, 0, 2) # [46, 50, 768]
-                    _x21 = self.model.visual.ln_post(_x21[:, 0, :]) # [46, 768]
+                    x_hybrid_global = x_hybrid_global.permute(1, 0, 2) # [46, 50, 768]
+                    x_hybrid_global = self.model.visual.ln_post(x_hybrid_global[:, 0, :]) # [46, 768]
                     if self.model.visual.proj is not None:
-                        _x21 = _x21 @ self.model.visual.proj # [46, 512] 
-                    return _x1, _x21
+                        x_hybrid_global = x_hybrid_global @ self.model.visual.proj # [46, 512] 
+                    return x_hybrid_local + x_hybrid_global
 
 
         return x
